cce846665c9ea8338bba3e3f0ffa53ac56fe8722
ikejemba <kinecon12@gmail.com>
2024-06-06 12:52:12 -0700

    update

--- studybud/settings.py
+++ studybud/settings.py
@@ -78,16 +78,14 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
- #   }
-#}
-
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+DATABASES["default"] = dj_database_url.parse("postgres://testdb_oqdf_user:6GROOeHQXlEVPvSsuE4fgK5GOC7uKKnz@dpg-cpgsore3e1ms73amdvng-a.oregon-postgres.render.com/testdb_oqdf")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
